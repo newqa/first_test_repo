5c1b614fa71b1e139757198a2bf4e5d7e4fc382a
New QA <mpancewiczpuchalska@gmail.com>
2021-05-01 13:27:29 +0200

    xxx

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -7,7 +7,5 @@ public class Calculate {
 
         System.out.println(calc.add(1, 2));
         System.out.println(calc.subtract(3, 4));
-//        System.out.println(calc.multiply());
-//        System.out.println(calc.divide());
     }
 }
